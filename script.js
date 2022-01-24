719f686c87f2ee1699bc35ca9e000db35675f22b
Feleti Manu <feleti.manu@gmail.com>
2022-01-23 22:42:26 -0700

    Edited script.js to test Object-Oriented Programming. Will function Game objects with User and AI. Will work with cookies in the future for local game storage.

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function selection(id, isMarked)
 {
     alert("Clicked " + id);
 }
+
+class Game {
+    user = null;
+    user2 = null;
+
+    constructor(){
+        this.user = 'user1';
+        this.user2 = 'user2';
+    }
+
+    printUser(){
+        alert(this.user);
+        alert(this.user2);
+    }
+}
+
+const game = new Game();
+game.printUser();
