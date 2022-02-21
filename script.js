26092d583ed976a285f4fcd0be6b08fc6942b2af
Feleti Manu <feleti.manu@gmail.com>
2022-02-21 01:06:20 -0700

    TTT-2, added functionality to the game with new reset button and wins. > > For TTT-2, not complete as of yet. Do not merge. > Added functions for Game class to make checks against the game board. Checks if user wins for Columns, Rows, Diagonals, and alerts thems. > > Added Reset button and function to Game board in case resetting board was needed. Resetting board reloads page. > > NEEDS > Reformat end-game checks for AI inputs and wins/user losses as well as reformatting grid multi-dimensional array for better checks against user/ai inputs. Eg. instead of isMarked and Owner keys, use values 1 or 2 to differentiate user/ai input as well as t/f marking.

--- script.js
+++ script.js
@@ -28,8 +28,89 @@ class Game {
         let id = parseInt(stringId);
         this.grid[id]['isMarked'] = 1;
         this.grid[id]['owner'] = this.player1;
+        this.checks();
     }
 
+    checks(){
+        this.checkColumns();
+        this.checkRows();
+        this.checkDiagonals();
+    }
+
+    checkColumns(){
+        if((this.grid[1]['isMarked'] == 1)
+        && (this.grid[4]['isMarked'] == 1)
+        && (this.grid[7]['isMarked'] == 1)){
+            this.win();
+        } else if (
+            (this.grid[2]['isMarked'] == 1)
+            && (this.grid[5]['isMarked'] == 1)
+            && (this.grid[8]['isMarked'] == 1)
+        ){
+            this.win();
+        } else if (
+            (this.grid[3]['isMarked'] == 1)
+            && (this.grid[6]['isMarked'] == 1)
+            && (this.grid[9]['isMarked'] == 1)
+        ){
+            this.win();
+        }
+    }
+
+    checkRows(){
+        if((this.grid[1]['isMarked'] == 1)
+        && (this.grid[2]['isMarked'] == 1)
+        && (this.grid[3]['isMarked'] == 1)){
+            this.win();
+        } else if (
+            (this.grid[4]['isMarked'] == 1)
+            && (this.grid[5]['isMarked'] == 1)
+            && (this.grid[6]['isMarked'] == 1)
+        ){
+            this.win();
+        } else if (
+            (this.grid[7]['isMarked'] == 1)
+            && (this.grid[8]['isMarked'] == 1)
+            && (this.grid[9]['isMarked'] == 1)
+        ){
+            this.win();
+        }
+    }
+
+    checkDiagonals(){
+        if((this.grid[1]['isMarked'] == 1)
+        && (this.grid[5]['isMarked'] == 1)
+        && (this.grid[9]['isMarked'] == 1)){
+            this.win();
+        } else if (
+            (this.grid[3]['isMarked'] == 1)
+            && (this.grid[5]['isMarked'] == 1)
+            && (this.grid[7]['isMarked'] == 1)
+        ){
+            this.win();
+        }
+    }
+
+    reset(){
+        let reset = confirm('RESET?');
+        if(reset == true){
+            for (let i=1; i<=9; i++){
+                this.grid[i]["isMarked"] = 0;
+                this.grid[i]["owner"] = null;
+            }
+            window.location.reload();
+        }
+    }
+
+    win(){
+        alert('YOU WIN!');
+        this.reset();
+    }
+
+    loss(){
+        alert('YOU LOSE! SORRY BOUT IT');
+        this.reset();
+    }
 }
 
 const game = new Game();
