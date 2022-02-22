acf9dcf3c601d7bcb8f20f919242dd91fefa5524
Feleti Manu <feleti.manu@gmail.com>
2022-02-22 01:09:19 -0700

    TTT-2 with connections to TTT-3. Added AI gameplay, corrected format for classes, issues include undefined decisions by AI.
    Added AI Gameplay, for now it is only after palyer chooses, will add function to select who goes first and have a short
    wait time to give the effect that the AI is thinking. Also will add difficulty so that selections are not so random, but more precise for harder gameplay.
    
    Classes have been reformatted so that Player and AI classes are not children of the Game class and have easier access to the gameboard grid. The reformat changes click functions and added the decision function to AI. AI function decision changes styling and symbol for AI (blue O)
    
    ISSUES:
    Randomly has UNDEFINED as AI decision. Is most likely caused by randomizing the options available to the AI. May need to use different randomizing method since decimals can be tricky.
    
    NEEDS:
    - Fix undefined issue with AI decision
    - Add decision to select who goes first with flip a coin option.
    - Add menu options for better user GUI (TTT-4)

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-class Player {
-    symbol = null;
-
-    constructor(){
-        this.symbol = 'X';
-    }
-
-    clicked(id){
-        parent = document.getElementById(id);
-        parent.children[0].style.opacity = 1;
-    }
-}
-
 class Game {
-    player1 = null;
     grid = [];
 
     constructor(){
-        this.player1 = new Player();
+        this.ai = new AI();
         for (let i=1; i<=9; i++){
             this.grid[i] = [];
-            this.grid[i]["isMarked"] = 0;
-            this.grid[i]["owner"] = null;
+            this.grid[i] = 0;
         }
     }
 
-    clicked(stringId){
+    clicked(stringId, owner){
         let id = parseInt(stringId);
-        this.grid[id]['isMarked'] = 1;
-        this.grid[id]['owner'] = this.player1;
+        this.grid[id] = owner;
         this.checks();
     }
 
@@ -38,56 +22,104 @@ class Game {
     }
 
     checkColumns(){
-        if((this.grid[1]['isMarked'] == 1)
-        && (this.grid[4]['isMarked'] == 1)
-        && (this.grid[7]['isMarked'] == 1)){
+        if((this.grid[1] == 1)
+        && (this.grid[4] == 1)
+        && (this.grid[7] == 1)){
             this.win();
         } else if (
-            (this.grid[2]['isMarked'] == 1)
-            && (this.grid[5]['isMarked'] == 1)
-            && (this.grid[8]['isMarked'] == 1)
+            (this.grid[2] == 1)
+            && (this.grid[5] == 1)
+            && (this.grid[8] == 1)
         ){
             this.win();
         } else if (
-            (this.grid[3]['isMarked'] == 1)
-            && (this.grid[6]['isMarked'] == 1)
-            && (this.grid[9]['isMarked'] == 1)
+            (this.grid[3] == 1)
+            && (this.grid[6] == 1)
+            && (this.grid[9] == 1)
         ){
             this.win();
+        }
+
+        if((this.grid[1] == 2)
+        && (this.grid[4] == 2)
+        && (this.grid[7] == 2)){
+            this.win();
+        } else if (
+            (this.grid[2] == 2)
+            && (this.grid[5] == 2)
+            && (this.grid[8] == 2)
+        ){
+            this.win();
+        } else if (
+            (this.grid[3] == 2)
+            && (this.grid[6] == 2)
+            && (this.grid[9] == 2)
+        ){
+            this.loss();
         }
     }
 
     checkRows(){
-        if((this.grid[1]['isMarked'] == 1)
-        && (this.grid[2]['isMarked'] == 1)
-        && (this.grid[3]['isMarked'] == 1)){
+        if((this.grid[1] == 1)
+        && (this.grid[2] == 1)
+        && (this.grid[3] == 1)){
             this.win();
         } else if (
-            (this.grid[4]['isMarked'] == 1)
-            && (this.grid[5]['isMarked'] == 1)
-            && (this.grid[6]['isMarked'] == 1)
+            (this.grid[4] == 1)
+            && (this.grid[5] == 1)
+            && (this.grid[6] == 1)
         ){
             this.win();
         } else if (
-            (this.grid[7]['isMarked'] == 1)
-            && (this.grid[8]['isMarked'] == 1)
-            && (this.grid[9]['isMarked'] == 1)
+            (this.grid[7] == 1)
+            && (this.grid[8] == 1)
+            && (this.grid[9] == 1)
         ){
             this.win();
+        }
+
+        if((this.grid[1] == 2)
+        && (this.grid[2] == 2)
+        && (this.grid[3] == 2)){
+            this.loss();
+        } else if (
+            (this.grid[4] == 2)
+            && (this.grid[5] == 2)
+            && (this.grid[6] == 2)
+        ){
+            this.loss();
+        } else if (
+            (this.grid[7] == 2)
+            && (this.grid[8] == 2)
+            && (this.grid[9] == 2)
+        ){
+            this.loss();
         }
     }
 
     checkDiagonals(){
-        if((this.grid[1]['isMarked'] == 1)
-        && (this.grid[5]['isMarked'] == 1)
-        && (this.grid[9]['isMarked'] == 1)){
+        if((this.grid[1] == 1)
+        && (this.grid[5] == 1)
+        && (this.grid[9] == 1)){
             this.win();
         } else if (
-            (this.grid[3]['isMarked'] == 1)
-            && (this.grid[5]['isMarked'] == 1)
-            && (this.grid[7]['isMarked'] == 1)
+            (this.grid[3] == 1)
+            && (this.grid[5] == 1)
+            && (this.grid[7] == 1)
         ){
             this.win();
+        }
+
+        if((this.grid[1] == 2)
+        && (this.grid[5] == 2)
+        && (this.grid[9] == 2)){
+            this.loss();
+        } else if (
+            (this.grid[3] == 2)
+            && (this.grid[5] == 2)
+            && (this.grid[7] == 2)
+        ){
+            this.loss();
         }
     }
 
@@ -95,8 +127,7 @@ class Game {
         let reset = confirm('RESET?');
         if(reset == true){
             for (let i=1; i<=9; i++){
-                this.grid[i]["isMarked"] = 0;
-                this.grid[i]["owner"] = null;
+                this.grid[i] = 0;
             }
             window.location.reload();
         }
@@ -113,18 +144,67 @@ class Game {
     }
 }
 
+class AI {
+    game = null;
+    symbol = null;
+
+    constructor(game){
+        this.symbol = 'O';
+        this.game = game;
+    }
+
+    clicked(id){
+        let parent = document.getElementById(id);
+        parent.children[0].children[0].innerHTML = this.symbol;
+        parent.children[0].style.opacity = 1;
+        parent.children[0].children[0].style.color = '#00F';
+    }
+
+    decide(){
+        let options = [];
+        for(let i=1; i<=9; i++){
+            if(this.game.grid[i] == 0)
+                options[i] = i;
+        }
+        alert(options);
+        let decision = options[Math.floor(Math.random()*options.length)];
+        alert(decision);
+        this.game.clicked(decision, 2);
+        this.clicked(decision);
+    }
+}
+
+class Player {
+    game = null;
+    symbol = null;
+
+    constructor(game, ai){
+        this.symbol = 'X';
+        this.game = game;
+        this.ai = ai;
+    }
+
+    clicked(id){
+        let parent = document.getElementById(id);
+        parent.children[0].style.opacity = 1;
+        this.ai.decide();
+    }
+}
+
 const game = new Game();
+const ai = new AI(game);
+const player = new Player(game, ai);
 
 function selection(id) {
-    game.clicked(id);
-    game.player1.clicked(id);
+    game.clicked(id, 1);
+    player.clicked(id);
 }
 
 function testGrid(){
     let test = '';
     for(let i=1; i<=9; i++){
         test += '(' + i + ', ';
-        test += game.grid[i]['isMarked'].toString();
+        test += game.grid[i].toString();
         test += '), ';
     }
     alert(test);
